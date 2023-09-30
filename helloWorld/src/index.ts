f07026f4f5fdeaf62b032d4a38c8e85867568b16
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-30 10:40:31 -0400

    validação das variáveis de ambiente

--- helloWorld/src/index.ts
+++ helloWorld/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express'
+import validateEnv from './utils/validateEnv'
 import dotenv from 'dotenv'
 
 dotenv.config()
+validateEnv()
+
 const app = express()
 const PORT = process.env.PORT || 3333
 
