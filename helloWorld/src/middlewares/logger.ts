0996304bb7ddf680d6c2e98e532baf7805c1db23
shl0mo <salomao.dacostacruz@gmail.com>
2023-10-03 20:52:58 -0400

    logger - completo

--- helloWorld/src/middlewares/logger.ts
+++ helloWorld/src/middlewares/logger.ts
@@ -9,17 +9,19 @@ const PASTA_LOGS = process.env.PASTA_LOGS
 function logger(tipo: 'simples' | 'completo') {
 	if (tipo === 'simples') {
 		return (req: Request, res: Response, next: NextFunction) => {
-			console.log(new Date().toISOString(), req.url, req.method)
-			fs.writeFile('logs.txt', 'log', function (err) {
+			const log = `${new Date().toISOString()} ${req.url} ${req.method}\n`
+			fs.appendFile(`${__dirname}/logs.txt`, log, (err) => {
 				if (err) throw err
 				next()
 			})
-			next()
 		}
 	} else {
 		return (req: Request, res: Response, next: NextFunction) => {
-			console.log(new Date().toISOString(), req.url, req.method, req.httpVersion, req.get('User-Agent'))
-			next()
+			const log = `${new Date().toISOString()} ${req.url} ${req.method} ${req.httpVersion} ${req.get('User-Agent')}\n`
+			fs.appendFile(`${__dirname}/logs.txt`, log, (err) => {
+				if (err) throw err
+				next()
+			})
 		}
 	}
 
--- helloWorld/src/router/router.ts
+++ helloWorld/src/router/router.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express'
+import { loremIpsum } from 'lorem-ipsum'
 
 const router = express.Router()
+
+router.get('/lorem/:paragrafos', (req: Request, res: Response) => {
+	res.send(loremIpsum({
+		count: parseInt(req.params.paragrafos)
+	}))
+})
 
 router.get('/', (req: Request, res: Response) => {
 	res.end('Início')	
