602e1ecbb2847c4bbb6b5a5dc7cfae782d0b920a
shl0mo <salomao.dacostacruz@gmail.com>
2023-10-05 19:50:37 -0400

    exercicio 7

--- helloWorld/src/index.ts
+++ helloWorld/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
+import { engine } from 'express-handlebars'
 import validateEnv from './utils/validateEnv'
 import dotenv from 'dotenv'
-// import morgan from 'morgan'
 import logger from './middlewares/logger'
 import router from './router/router'
 
@@ -11,6 +11,13 @@ validateEnv()
 const app = express()
 const PORT = process.env.PORT || 3333
 
+// console.log(`${__dirname}/helpers/helpers.ts`)
+
+app.engine('handlebars', engine({
+	helpers: require(`${__dirname}/helpers/helpers.ts`)
+}))
+app.set('view engine', 'handlebars')
+app.set('views', `${__dirname}/views`)
 
 app.use(logger('completo'))
 app.use(router)
@@ -19,6 +26,8 @@ app.use(router)
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello world!')
 })
+
+
 
 app.listen(PORT, () => {
 	console.log(`Express app iniciada na porta ${PORT}.`)
--- helloWorld/src/router/router.ts
+++ helloWorld/src/router/router.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express'
+import { engine } from 'express-handlebars'
 import { loremIpsum } from 'lorem-ipsum'
 
 const router = express.Router()
+
 
 router.get('/lorem/:paragrafos', (req: Request, res: Response) => {
 	res.send(loremIpsum({
@@ -15,6 +17,46 @@ router.get('/', (req: Request, res: Response) => {
 
 router.get('/about', (req: Request, res: Response) => {
 	res.send('Sobre')
+})
+
+
+router.get('/hb1', (req: Request, res: Response) => {
+	res.render('hb1', {
+		mensagem: 'Olá, você está aprendendo Express + HBS!',
+		layout: false,
+	})
+})
+
+router.get('/hb2', (req: Request, res: Response) => {
+	res.render('hb2', {
+		poweredByNodejs: true,
+		name: 'Express',
+		type: 'Framework',
+		layout: false,
+	})
+})
+
+router.get('/hb3', (req: Request, res: Response) => {
+	const profes = [
+		{ nome: 'Daivd Fernandes', sala: 1238 },
+		{ nome: 'Horacio Fernandes', sala: 1233 },
+		{ nome: 'Edleno Moura', sala: 1236 },
+		{ nome: 'Elaine Harada', sala: 1232 }
+	]
+	res.render('hb/hb3', { profes, layout: false })
+})
+
+router.get('/hb4', (req: Request, res: Response) => {
+	const technologies = [
+		{  name: 'Express', type: 'Framework', poweredByNodejs: true },
+		{  name: 'Laravel', type: 'Framework', poweredByNodejs: false },
+		{  name: 'React', type: 'Library', poweredByNodejs: true },
+		{  name: 'Handlebars', type: 'Engine View', poweredByNodejs: true },
+		{  name: 'Django', type: 'Framework', poweredByNodejs: false },
+		{  name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
+		{  name: 'Sequelize', type: 'ORM toll', poweredByNodejs: true },
+	]
+	res.render('hb4', { technologies, layout: false })
 })
 
 export default router
